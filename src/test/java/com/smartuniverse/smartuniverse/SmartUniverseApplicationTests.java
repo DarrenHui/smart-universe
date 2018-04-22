f164aeefdf39c96a00310f382fa4756f37b8a7dc
DarrenHui <38596558+DarrenHui@users.noreply.github.com>
2018-04-22 20:52:24 +0800

    Initial commit

---
+++ src/test/java/com/smartuniverse/smartuniverse/SmartUniverseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smartuniverse.smartuniverse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartUniverseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
